935309151fc5518b9339b39598b1848471c0e6b7
rohan m rayaraddi <rohan2962@gmail.com>
2018-04-09 10:27:06 +0530

    Add files via upload

---
+++ plotter.h
@@ -0,0 +1,14 @@
+#define print 0
+#define help 1
+#define plot 2
+#define SIN 3
+#define COS 4
+#define TAN 5
+#define ABS 6
+#define POW 7
+#define LOG 8
+#define EXIT 9
+#define VAR 10
+#define NUM 11
+
+
